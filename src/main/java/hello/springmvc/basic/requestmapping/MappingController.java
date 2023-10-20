1673968a433be4bce7921ffae7d2e2e9f7c8cdaf
choihyeji <hyejichoi14@gmail.com>
2023-10-21 01:05:50 +0900

    Controller mapping, log practice

---
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,53 @@
+package hello.springmvc.basic.requestmapping;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+@Slf4j
+public class MappingController {
+
+    /**
+     * 기본 요청
+     * 둘다 허용 /hello-basic, /hello-basic/
+     * HTTP 메서드 모두 허용 GET, HEAD, POST, PUT, PATCH, DELETE
+     */
+    @RequestMapping(value = {"/hello-basic", "/hello-go"},
+            method = GET) //@RequestMapping은 method 설정 없으면 post,get,put,delete 다 됨
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    /**
+     * 편리한 축약 애노테이션 (코드보기)
+     *
+     * @GetMapping
+     * @PostMapping
+     * @PutMapping
+     * @DeleteMapping
+     * @PatchMapping
+     */
+    @GetMapping(value = "/mapping-get-v2")
+    public String mappingGetV2() {
+        log.info("mapping-get-v2");
+        return "ok";
+    }
+
+    /**
+     * PathVariable 사용
+     * 변수명이 같으면 생략 가능
+     *
+     * @PathVariable("userId") String userId -> @PathVariable userId
+     *
+     */
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mappingPath userId={}", data);
+        return "ok";
+    }
+}
